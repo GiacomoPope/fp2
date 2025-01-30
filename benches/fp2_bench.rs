c06bc88df4fdb4db98c1c35a88fd095c9be105ce
Giacomo Pope <giacomopope@gmail.com>
2025-01-30 11:17:39 +0000

    remove two parameters from macro

--- benches/fp2_bench.rs
+++ benches/fp2_bench.rs
@@ -108,14 +108,12 @@ mod bench_fp_251_ext {
     // Macro input generated with scripts/gen_fp.sage
     // p = 5*2^248 - 1
     fp2_rs::define_fp_core!(
-        type_name = Fp251,
-        words = 4_usize,
-        bit_len = 251_usize,
+        typename = Fp251,
         modulus = [
-            0xFFFFFFFFFFFFFFFF,
-            0xFFFFFFFFFFFFFFFF,
-            0xFFFFFFFFFFFFFFFF,
-            0x04FFFFFFFFFFFFFF
+            0xFFFFFFFFFFFFFFFF_u64,
+            0xFFFFFFFFFFFFFFFF_u64,
+            0xFFFFFFFFFFFFFFFF_u64,
+            0x04FFFFFFFFFFFFFF_u64
         ],
         half_modulus = [
             0x0000000000000000,
@@ -185,7 +183,7 @@ mod bench_fp_251_ext {
     // Contents are opaque, all functions are constant-time.
     // Macro input generated with scripts/gen_fp.sage
     fp2_rs::define_fp2_core!(
-        type_name = Fp251Ext,
+        typename = Fp251Ext,
         base_field = Fp251,
         nqr_re = [
             0x0000000000000100,
--- tests/test_fields.rs
+++ tests/test_fields.rs
@@ -7,10 +7,12 @@ mod tests {
         // Macro input generated with scripts/gen_fp.sage
         // p = 2^74 * 3^41 - 1
         fp2_rs::define_fp_core!(
-            type_name = Fp139,
-            words = 3_usize,
-            bit_len = 139_usize,
-            modulus = [0xFFFFFFFFFFFFFFFF, 0xA873D9ED7EE18BFF, 0x00000000000007E8],
+            typename = Fp139,
+            modulus = [
+                0xFFFFFFFFFFFFFFFF_u64,
+                0xA873D9ED7EE18BFF_u64,
+                0x00000000000007E8_u64
+            ],
             half_modulus = [0x0000000000000000, 0x5439ECF6BF70C600, 0x00000000000003F4],
             mont_r = [0x00205E71C12F4F15, 0x0C05B368494B8400, 0x00000000000006C0],
             neg_r = [0xFFDFA18E3ED0B0EA, 0x9C6E2685359607FF, 0x0000000000000128],
@@ -34,7 +36,7 @@ mod tests {
         // Contents are opaque, all functions are constant-time.
         // Macro input generated with scripts/gen_fp.sage
         fp2_rs::define_fp2_core!(
-            type_name = Fp139Ext,
+            typename = Fp139Ext,
             base_field = Fp139,
             nqr_re = [0x0143B0718BD916DA, 0x349A32A6E5E6C800, 0x000000000000043B]
         );
@@ -50,14 +52,12 @@ mod tests {
         // Macro input generated with scripts/gen_fp.sage
         // p = 5*2^248 - 1
         fp2_rs::define_fp_core!(
-            type_name = Fp251,
-            words = 4_usize,
-            bit_len = 251_usize,
+            typename = Fp251,
             modulus = [
-                0xFFFFFFFFFFFFFFFF,
-                0xFFFFFFFFFFFFFFFF,
-                0xFFFFFFFFFFFFFFFF,
-                0x04FFFFFFFFFFFFFF
+                0xFFFFFFFFFFFFFFFF_u64,
+                0xFFFFFFFFFFFFFFFF_u64,
+                0xFFFFFFFFFFFFFFFF_u64,
+                0x04FFFFFFFFFFFFFF_u64
             ],
             half_modulus = [
                 0x0000000000000000,
@@ -127,13 +127,13 @@ mod tests {
         // Contents are opaque, all functions are constant-time.
         // Macro input generated with scripts/gen_fp.sage
         fp2_rs::define_fp2_core!(
-            type_name = Fp251Ext,
+            typename = Fp251Ext,
             base_field = Fp251,
             nqr_re = [
-                0x0000000000000100,
-                0x0000000000000000,
-                0x0000000000000000,
-                0x0000000000000000
+                0x0000000000000100_u64,
+                0x0000000000000000_u64,
+                0x0000000000000000_u64,
+                0x0000000000000000_u64
             ]
         );
 
@@ -146,16 +146,14 @@ mod tests {
         // Contents are opaque, all functions are constant-time.
         // Macro input generated with scripts/gen_fp.sage
         fp2_rs::define_fp_core!(
-            type_name = Fp383,
-            words = 6_usize,
-            bit_len = 383_usize,
+            typename = Fp383,
             modulus = [
-                0xFFFFFFFFFFFFFFFF,
-                0xFFFFFFFFFFFFFFFF,
-                0xFFFFFFFFFFFFFFFF,
-                0xFFFFFFFFFFFFFFFF,
-                0xFFFFFFFFFFFFFFFF,
-                0x40FFFFFFFFFFFFFF
+                0xFFFFFFFFFFFFFFFF_u64,
+                0xFFFFFFFFFFFFFFFF_u64,
+                0xFFFFFFFFFFFFFFFF_u64,
+                0xFFFFFFFFFFFFFFFF_u64,
+                0xFFFFFFFFFFFFFFFF_u64,
+                0x40FFFFFFFFFFFFFF_u64
             ],
             half_modulus = [
                 0x0000000000000000,
@@ -243,7 +241,7 @@ mod tests {
         // Contents are opaque, all functions are constant-time.
         // Macro input generated with scripts/gen_fp.sage
         fp2_rs::define_fp2_core!(
-            type_name = Fp383Ext,
+            typename = Fp383Ext,
             base_field = Fp383,
             nqr_re = [
                 0x0000000000000017,
