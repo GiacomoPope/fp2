0bbe71eb8546d6871a0cb7bc267a3fb4ccbf54ab
Giacomo Pope <giacomopope@gmail.com>
2025-05-08 14:35:52 +0100

    add more methods to trait

--- src/fq.rs
+++ src/fq.rs
@@ -12,6 +12,7 @@ pub trait Fq:
     + Div<Output = Self>
     + DivAssign
 {
+    // Constants for internal computations
     const ENCODED_LENGTH: usize;
 
     // Precomputed elements in the finite field
@@ -28,37 +29,58 @@ pub trait Fq:
     fn iszero(self) -> u32;
     fn equals(self, rhs: &Self) -> u32;
 
-    // Specialised arithmetic operations
-    fn set_mul_small(&mut self, k: i32);
-    fn square(self) -> Self;
-    fn set_invert(&mut self);
-    fn sqrt(self) -> (Self, u32);
-    fn fourth_root(self) -> (Self, u32);
+    // Negate in-place, kinda like "NegAssign"
+    fn set_neg(&mut self);
 
     // Multiplication by constants
+    fn set_half(&mut self);
     fn set_mul2(&mut self);
     fn set_mul3(&mut self);
     fn set_mul4(&mut self);
     fn set_mul8(&mut self);
-    fn set_half(&mut self);
+
     fn half(self) -> Self;
     fn mul2(self) -> Self;
     fn mul3(self) -> Self;
     fn mul4(self) -> Self;
     fn mul8(self) -> Self;
 
+    // Specialised arithmetic operations
+    fn set_conjugate(&mut self);
+    fn set_mul_small(&mut self, k: i32);
+    fn set_square(&mut self);
+    fn set_invert(&mut self);
+    fn set_pow(&mut self, e: &[u8], ebitlen: usize);
+    fn set_pow_ext(&mut self, e: &[u8], eoff: usize, ebitlen: usize);
+    fn set_pow_u64(&mut self, e: u64, ebitlen: usize);
+    fn set_pow_u64_vartime(&mut self, e: u64);
+
+    fn conjugate(self) -> Self;
+    fn mul_small(self, k: i32) -> Self;
+    fn square(self) -> Self;
+    fn invert(self) -> Self;
+    fn pow(self, e: &[u8], ebitlen: usize) -> Self;
+    fn pow_ext(self, e: &[u8], eoff: usize, ebitlen: usize) -> Self;
+    fn pow_u64(&mut self, e: u64, ebitlen: usize) -> Self;
+    fn pow_u64_vartime(&mut self, e: u64) -> Self;
+
+    // Square roots and Fourth Roots
+    fn set_sqrt(&mut self) -> u32;
+    fn set_fourth_root(&mut self) -> u32;
+    fn sqrt(self) -> (Self, u32);
+    fn fourth_root(self) -> (Self, u32);
+
     // Compute the legendre symbol (a/p)
     fn legendre(self) -> i32;
 
-    // Specialised operations for Fp2 arithmetic
-    fn sum_of_products(a1: &Self, b1: &Self, a2: &Self, b2: &Self) -> Self;
-    fn difference_of_products(a1: &Self, b1: &Self, a2: &Self, b2: &Self) -> Self;
+    // Batch inversion
+    fn batch_invert(xx: &mut [Self]);
 
     // Constant time manipulations
-    fn select(a: &Self, b: &Self, ctl: u32) -> Self;
     fn set_select(&mut self, a: &Self, b: &Self, ctl: u32);
     fn set_cond(&mut self, rhs: &Self, ctl: u32);
     fn set_condneg(&mut self, ctl: u32);
+    fn select(a: &Self, b: &Self, ctl: u32) -> Self;
     fn condswap(a: &mut Self, b: &mut Self, ctl: u32);
 
     // Encoding and Decoding
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+// We include these so we can have things like
+// fn encode(self) -> [u8; Self::ENCODED_LENGTH];
+// defined within the Fq trait
 #![allow(incomplete_features)]
 #![feature(generic_const_exprs)]
 
